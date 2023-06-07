c56a048627b4e355d8c1c932d7f863e9c44b2dfe
owesleymauricio <135478971+owesleymauricio@users.noreply.github.com>
2023-06-06 21:34:11 -0300

    innitialCommit

---
+++ script.js
@@ -0,0 +1,8 @@
+function clicou() {
+    document.querySelector('form').style.opacity = 1
+    document.querySelector('.container').style.opacity = 0.1
+}
+function back() {
+    document.querySelector('form').style.opacity = 0
+    document.querySelector('.container').style.opacity = 1
+}
